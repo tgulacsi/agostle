ab3e757d76b3a9d4c800d89a944403729380b6af
Tamás Gulácsi <tgulacsi78@gmail.com>
2017-11-19 16:27:13 +0100

    add root key

---
+++ tuf_rootkey.go
@@ -0,0 +1,7 @@
+// Copyright 2017 The Agostle Authors. All rights reserved.
+// Use of this source code is governed by an Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+const defaultRootKeys = `[{"keytype":"ed25519","keyval":{"public":"96593b24cc182facfa7c96a3826e098c3abce04faa1254664d24ce879b6b833c"}}]`
